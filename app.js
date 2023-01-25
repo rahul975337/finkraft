6580c6e7f7a095cac0414b50099e6c996e8dd0b0
Rahul Tilwani <tilwani03@gmail.com>
2023-01-25 23:42:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const fun = (arr, sum) => {
+   let map ={}
+   for (let i = 0; i < arr.length; i++) {
+    let left = target-arr[i]
+   }
+  return res;
+};
+
+let arr = [1,3,10,11,14];
+let sum = 13
+let res = fun(arr,sum)
+
+for (let i = 0; i < res.length; i++) {
+    const element = res[i];
+    console.log(element);
+
+}
